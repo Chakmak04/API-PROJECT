a5a137a274e6b04e872010b7ba20abcdbe300f8f
My name <chakmak04@gmail.com>
2023-04-20 20:18:24 +0200

    Task is done HR

---
+++ src/test/java/com/cydeo/Day2/HrGetRequests.java
@@ -0,0 +1,64 @@
+package com.cydeo.Day2;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HrGetRequests {
+
+    //BeforeAll is an annotation equals to @BeforeClass in testNG
+    //We use with static method name
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we don't need to type each http method.
+        RestAssured.baseURI = "http://3.90.70.155:1000/ords/hr";
+
+    }
+
+    @DisplayName("Get request to /regions")
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.get("/regions");
+
+        //print status code
+        System.out.println(response.statusCode());
+
+    }
+
+    /*
+    Given accept type is application/json
+    when user sends get request to /regions/2
+    Then response status code must be 200
+    and content type equals to application
+    and response body contains Americas
+     */
+
+    @DisplayName("Get request to/regions/2")
+    @Test
+    public void test2(){
+
+        Response response = RestAssured.get("/regions/2");
+
+        //Verify status code
+        Assertions.assertEquals(200,response.statusCode());
+
+        //Verify content type
+        Assertions.assertEquals("application/json",response.contentType());
+
+        response.prettyPrint();
+
+        //Verify body contains Americas
+        Assertions.assertEquals(response.body().asString().contains("Americas"),true);
+
+
+
+
+
+    }
+}
+
+
